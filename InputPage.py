bf113827c60d438be33c1bf0dff876211b6bd36d
Brian <brian.simpkins@gmail.com>
2022-10-05 18:11:55 -0700

    Creating class + file for the input page

---
+++ InputPage.py
@@ -0,0 +1,13 @@
+# import our libraries
+import sys
+import PyQt5.QtWidgets as qw
+import PyQt5.QtCore as qc
+
+class InputPage:
+    def __init__():
+        app = qc.QCoreApplication.instance()
+        if app is None:
+            app = qc.QApplication(sys.argv)
+            app.setStyle("Fusion")
+
+
